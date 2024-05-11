ab8a6dc405cd5cdac89eabec32ffe3eaed3ffa28
kyle <averytkyle@gmail.com>
2024-05-11 16:30:06 -0400

    switch

--- aa05-dynamic-array/dynamic-array.js
+++ aa05-dynamic-array/dynamic-array.js
@@ -1,24 +1,31 @@
 class DynamicArray {
 
   constructor(defaultSize = 4) {
-
-    // Your code here 
+    this.capacity = defaultSize
+    this.length = 0
+    this.data = Array(this.capacity)
   }
 
   read(index) {
-
-    // Your code here 
+    return this.data[index]
   }
 
   push(val) {
-
-    // Your code here 
+    this.data[this.length] = val
+    this.length++
   }
 
 
   pop() {
-
-    // Your code here 
+    if (this.length === 0) {
+      return undefined
+    }
+    let result = this.data[this.length - 1]
+    this.data.length - 1
+    if(this.length > 0) {
+      this.length--
+    }
+    return result
   }
 
   shift() {
@@ -27,8 +34,11 @@ class DynamicArray {
   }
 
   unshift(val) {
-
-    // Your code here 
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1]
+    }
+    this.length++
+    this.data[0] = val
   }
 
   indexOf(val) {
